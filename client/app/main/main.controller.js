a4f7722c189ef993d4403e18b24abd4d42917e94
Andrew VanVlack <A.VanVlack@Gmail.com>
2015-11-04 06:46:32 -0800

    all user storys finished

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,23 +2,74 @@
 
 angular.module('vanvlackStockApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.stockData = [];
 
-    $http.get('/api/stocks').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('stock', $scope.awesomeThings);
+
+    $http.get('/api/stocks').success(function(stockData) {
+      $scope.stockData = stockData;
+      socket.syncUpdates('stock', $scope.stockData, function(event, item, array){
+        console.log(event);
+        if (event === 'created'){
+          var xsData = {};
+          xsData[item.code] = 'x' + item.code;
+          $scope.chart.load({
+              xs: xsData,
+              columns: [
+                ['x' + item.code].concat(item.dates),
+                [item.code].concat(item.closing_price)
+              ]
+          });
+        }
+        if (event === 'deleted'){
+          $scope.chart.unload({
+            ids: [item.code]
+          });
+        }
+      });
+      var y = [];
+      var x = [];
+      var xsData = {};
+      $scope.stockData.forEach(function(item){
+        y.push([item.code].concat(item.closing_price));
+        x.push(['x' + item.code].concat(item.dates));
+        xsData[item.code] = 'x' + item.code
+      });
+      $scope.chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          xs: xsData,
+          columns: x.concat(y)
+        },
+        axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d',
+                rotate: 90,
+                fit: false
+
+            }
+          }
+        },
+        point: {
+          show: false
+        },
+        zoom: {
+          enabled: true
+        }
+      });
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addStock = function() {
+      if($scope.newStock === '') {
         return;
       }
-      $http.post('/api/stocks', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/stocks', { name: $scope.newStock });
+      $scope.newStock = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/stocks/' + thing._id);
+    $scope.deleteStock = function(stock) {
+      $http.delete('/api/stocks/' + stock._id);
     };
 
     $scope.$on('$destroy', function () {
